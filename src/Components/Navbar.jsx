78fb09dead8f22c38ae36aa77b4a8922327104ef
Mohhit6075 <mohitsingh6266@gmail.com>
2024-08-09 19:32:21 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,13 @@
-import './Styles/Navbar.css'
+import Drawer from "./Nested Components/Drawer/Drawer";
+
 const Navbar = () => {
   return (
     <>
-      <div id="navbar" className="w-full mx-auto  md:flex justify-around items-center text-[#ddd] pt-4">
+      <div id="navbar" className="w-full mx-auto flex justify-around items-center text-[#ddd] pt-4">
         <a href="https://www.istemanit.in/" target="blank">
           <img src="/Images/Logo.svg" alt="logo" />
         </a>
-        <div id="nav-content" className=" flex items-center gap-2 md:gap-4 lg:gap-6 xl:gap-10 font-medium text-lg ">
+        <div className="items-center hidden md:flex lg:flex xl:flex gap-2 md:gap-4 lg:gap-6 xl:gap-10 font-medium text-lg">
           <a href="/" className="group">
             Home
             <div className="w-0 h-[1px] bg-[#6054aa] rounded-full group-hover:w-full transition-all duration-500"></div>
@@ -36,8 +37,8 @@ const Navbar = () => {
             <div className="w-0 h-[1px] bg-[#6054aa] rounded-full group-hover:w-full transition-all duration-500"></div>
           </a>
         </div>
-        <div className='hamburger'>
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 18L20 18" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path> <path d="M4 12L20 12" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path> <path d="M4 6L20 6" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path> </g></svg>
+        <div className="block md:hidden lg:hidden xl:hidden">
+          <Drawer />
         </div>
       </div>
     </>
